81ce1903af83424b78a23836047adbd3692820ad
John Nguyen <hello@jnguyen.me>
2015-10-09 18:44:35 -0700

    add: git config

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 dotfiles="$HOME/.dotfiles"
 
-# setup vim
+# setup vim config
 mkdir -pv $dotfiles/vim/bundle
 ln -s $dotfiles/vim ~/.vim
 ln -s $dotfiles/vimrc ~/.vimrc
@@ -8,3 +8,7 @@ if [ ! -e $HOME/.vim/bundle/Vundle.vim ]; then
   git clone git@github.com:gmarik/Vundle.vim.git $dotfiles/vim/bundle/Vundle.vim
 fi
 vim +PluginInstall +PluginClean! +qall
+
+# setup git config
+ln -s $dotfiles/gitconfig ~/.gitconfig
+ln -s $dotfiles/gitignore ~/.gitignore
