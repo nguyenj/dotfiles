436d0c67dd6ca1e66ca4761db9b8e049afaffd8c
John Nguyen <hello@jnguyen.me>
2016-01-24 10:40:23 -0800

    Ignore case for bash completion

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 dotfiles="$HOME/.dotfiles"
 
 # setup tmux
+echo "Setting up tmux"
 ln -s $dotfiles/tmux ~/.tmux.conf
 
 # setup vim config
+echo "Setting up vim"
 mkdir -pv $dotfiles/vim/bundle
 if [ ! -d ~/.vim ]; then
   ln -s $dotfiles/vim ~/.vim
@@ -17,10 +19,16 @@ fi
 vim +PluginInstall +PluginClean! +qall
 
 # setup git config
+echo "Setting up git"
 ln -s $dotfiles/gitconfig ~/.gitconfig
 ln -s $dotfiles/gitignore ~/.gitignore
 ln -s $dotfiles/git-prompt-colors ~/.git-prompt-colors.sh
 ln -s $dotfiles/git_diff_wrapper.sh ~/.git_diff_wrapper.sh
 
 # setup silversearch ignore file (ag)
+echo "Setting up ag"
 ln -s $dotfiles/agignore ~/.agignore
+
+# setup bash config
+echo "Setting up bash"
+ln -s $dotfiles/inputrc ~/.inputrc
