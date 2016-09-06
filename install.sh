61b64dee5ac6efb44860efc5a0b4a3b3e12fc3f3
John Nguyen <hello@jnguyen.me>
2016-09-06 08:18:41 -0700

    Rename bash_profile to bashrc

--- install.sh
+++ install.sh
@@ -2,9 +2,15 @@
 
 DOTFILES_ROOT=$(pwd -P)
 
-# Linking .zshrc
-cd $DOTFILES_ROOT/zsh/
-sh zsh.sh
+# # Linking .zshrc
+# cd $DOTFILES_ROOT/zsh/
+# sh zsh.sh
+
+# Setup bashrc
+if [ ! -e $HOME/.bashrc ]; then
+  echo "Linking .bashrc..."
+  ln -s $DOTFILES_ROOT/bash/bashrc $HOME/.bashrc
+fi
 
 # setup homebrew
 cd $DOTFILES_ROOT/homebrew
