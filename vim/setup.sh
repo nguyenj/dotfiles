12b976911ce26a9f9b5348115794644d5f8a4856
John Nguyen <hello@jnguyen.me>
2017-04-19 08:20:11 -0700

    Fix vim setup file context

--- vim/setup.sh
+++ vim/setup.sh
@@ -2,22 +2,22 @@
 
 set -e
 
-DOTFILES_ROOT=$(pwd -P)
+DOTFILES_VIM_ROOT=$(pwd -P)
 
 # setup vim config
 if [ ! -e $HOME/.vimrc ]; then
   echo "Symlink .vimrc"
   if [ ! -e $HOME/.vimrc ]; then
-    ln -s $DOTFILES_ROOT/vim/vimrc $HOME/.vimrc
+    ln -s $DOTFILES_VIM_ROOT/vimrc $HOME/.vimrc
   fi
 
   echo "Setting up vim plugins directory"
-  mkdir -pv $DOTFILES_ROOT/vim/vim/bundle
+  mkdir -pv $DOTFILES_VIM_ROOT/vim/bundle
   if [ ! -d $HOME/.vim ]; then
-    ln -s $DOTFILES_ROOT/vim/vim $HOME/.vim
+    ln -s $DOTFILES_VIM_ROOT/vim $HOME/.vim
   fi
   if [ ! -e $HOME/.vim/bundle/Vundle.vim ]; then
-    git clone git@github.com:gmarik/Vundle.vim.git $DOTFILES_ROOT/vim/vim/bundle/Vundle.vim
+    git clone git@github.com:gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
   fi
 
   echo "Installing vim plugins"
@@ -25,6 +25,6 @@ if [ ! -e $HOME/.vimrc ]; then
 
   if [ ! -e $HOME/.vimrc_background ]; then
     echo "Setting up vim colorscheme"
-    ln -s $DOTFILES_ROOT/vim/.vimrc_background $HOME/.vimrc_background
+    ln -s $DOTFILES_VIM_ROOT/vimrc_background $HOME/.vimrc_background
   fi
 fi
