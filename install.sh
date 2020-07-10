8a6dfeaf818f4b1ade71ebabb5e48fecf98217cc
John Nguyen <hello@jnguyen.me>
2020-07-10 17:12:10 -0400

    Add editorconfig vim plugin

--- install.sh
+++ install.sh
@@ -15,3 +15,10 @@ set -o pipefail
 DOTFILES_ROOT=$(cd `dirname $0` && pwd)
 
 sh $DOTFILES_ROOT/vim/setup.sh
+
+# Set .editorconfig globally
+# Check if .editorconfig exist
+if [ ! -e $HOME/.editorconfig ]; then
+  echo "Creating a symbolic link for .editorconfig"
+  ln -sf $DOTFILES_ROOT/.editorconfig $HOME/.editorconfig
+fi
